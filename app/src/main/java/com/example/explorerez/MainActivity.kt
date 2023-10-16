5cf06f61b60f13fbe9a5381c9207fd402a8e6fae
Muhamad Adib Minanurohman <Inanadib084@gmail.com>
2023-10-16 21:10:21 +0700

    Feat: BottomNavigation

--- app/src/main/java/com/example/explorerez/MainActivity.kt
+++ app/src/main/java/com/example/explorerez/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.explorerez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
+import androidx.fragment.app.Fragment
+import com.example.explorerez.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(BerandaFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.beranda_nav -> replaceFragment(BerandaFragment())
+                R.id.favorit_nav -> replaceFragment(FavoritFragment())
+                R.id.peta_nav -> replaceFragment(PetaFragment())
+                R.id.profil_nav -> replaceFragment(ProfilFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
