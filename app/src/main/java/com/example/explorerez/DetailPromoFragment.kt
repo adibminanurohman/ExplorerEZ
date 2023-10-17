f51043c203f34349f806139ec756b0b67f44cd34
Muhamad Adib Minanurohman <Inanadib084@gmail.com>
2023-10-18 06:51:59 +0700

    Feat: Beranda

--- app/src/main/java/com/example/explorerez/DetailPromoFragment.kt
+++ app/src/main/java/com/example/explorerez/DetailPromoFragment.kt
@@ -1,28 +1,26 @@
 package com.example.explorerez
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentManager
-import com.example.explorerez.databinding.FragmentBerandaBinding
 
-class BerandaFragment : Fragment() {
 
-    lateinit var binding: FragmentBerandaBinding
+class DetailPromoFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentBerandaBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
-        return binding.root
-
-
+        return inflater.inflate(R.layout.fragment_detail_promo, container, false)
     }
-
 
 }
